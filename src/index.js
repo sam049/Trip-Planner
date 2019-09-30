e9b9d63a7e19c6057557083547ebf88de340c100
Sam Miller <sammillerks@gmail.com>
2019-09-30 12:28:24 -0400

    got the map working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("I am bottom 10% coder!");
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtMDQ5IiwiYSI6ImNrMTZtM2h3NTE3a2gzaG8zZDVnZjdiZ2oifQ.vKBP8M0PnqhaTuNL9TpbWg';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
